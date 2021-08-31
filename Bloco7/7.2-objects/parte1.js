d4cb4964be56fdb7369a99078364270fad807612
Davio Lopes <davioplp@gmail.com>
2021-08-31 03:07:15 -0300

    exercicio 7.2

--- Bloco7/7.2-objects/parte1.js
+++ Bloco7/7.2-objects/parte1.js
@@ -1,3 +1,10 @@
+/* Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+Note que o parâmetro da função já está sendo passado na chamada da função.
+
+Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+Modifique o nome da pessoa compradora.
+Modifique o valor total da compra para R$ 50, 00. */
+
 const order = {
   name: 'Rafael Andrade',
   phoneNumber: '11-98763-1416',
@@ -42,15 +49,15 @@ const customerInfo = (order) => {
   const numero = order.address.number;
   const aprtamento = order.address.apartment;
   console.log(`ola ${deliveryPerson}, entrega para: ${destinatario}, Telefone: ${telefone},${erua}, N: ${numero}, AP: ${aprtamento}`);
-
-
 }
-
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
-
+  order.name = 'Luiz Silva';
+  const pizza = Object.keys(order.order.pizza);
+  const drinks = order.order.drinks.coke.type;
+  order.payment = '50,00';
+  console.log(`Olá ${order.name}, o total do seu pedido de ${pizza} e ${drinks} é ${order.payment}`);
 }
 
 orderModifier(order);
